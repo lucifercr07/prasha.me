ffa4cb88e55f7d977d5fc304712fd10d77c84c65
pshubham <pshubham@salesforce.com>
2024-03-15 22:02:07 +0530

    Added wait pages for other paths

---
+++ src/components/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Books() {
+    return (
+      <div className="bg-gray-900 min-h-screen flex flex-col justify-center items-center">
+        <h1 className="text-white text-4xl mb-4">Coming Soon!!</h1>
+        <p className="text-white text-lg mb-4">We're working hard to bring you something awesome. Stay tuned!!</p>
+        <p className="text-white text-lg mb-4">Back to <a href="/" className="text-green-400 border-b-4 border-green-500">home</a> for now!!</p>
+      </div>
+    );
+}
+
+export default Books;
